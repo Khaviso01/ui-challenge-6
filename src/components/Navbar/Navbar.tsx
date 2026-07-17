5b9409b23e1e781fbbe6bee891c482a2f0482284
Khaviso Vukeya <223881260@edu.vut.ac.za>
2026-07-17 22:09:28 +0200

    adjusted hamburger

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -13,27 +13,32 @@ function Navbar() {
       <div className="nav-container">
         <div className="navbar-logo">
           <img src={logo} alt="restaurant logo" />
-          <p>HEALTHY FOOD<br/>
+          <p>HEALTHY FOOD<br />
             HEALTHY LIFE
           </p>
         </div>
-        <ul className={isOpen?"navbar-menu active" : "navbar-menu"}>
-          <li><a href="#home">HOME</a></li>
-          <li><a href="#menu">MENU</a></li>
-          <li><a href="#pages">PAGES</a></li>
-          <li><a href="#blog">BLOG</a></li>
-          <li><a href="#contact">CONTACT US</a></li>
-          <button><HugeiconsIcon icon={Location04Icon} size={25} /></button>
-          <button><HugeiconsIcon icon={ShoppingCart02Icon} size={25}/></button>
-        </ul>
+        <div className='nav-right'>
+          <ul className={isOpen ? "navbar-menu active" : "navbar-menu"}>
+            <li><a href="#home">HOME</a></li>
+            <li><a href="#menu">MENU</a></li>
+            <li><a href="#pages">PAGES</a></li>
+            <li><a href="#blog">BLOG</a></li>
+            <li><a href="#contact">CONTACT US</a></li>
+            <li className="LocationCart">
+              <button><HugeiconsIcon icon={Location04Icon} size={25} /></button>
+              <button><HugeiconsIcon icon={ShoppingCart02Icon} size={25} /></button>
+            </li>
+          </ul>
 
+
+        </div>
         <div className="hamburger-menu">
           <Hamburger
-          toggled={isOpen}
-          toggle={setOpen}
-          size={24}
-          color="#fff"
-          rounded
+            toggled={isOpen}
+            toggle={setOpen}
+            size={24}
+            color="#fff"
+            rounded
           />
         </div>
       </div>
